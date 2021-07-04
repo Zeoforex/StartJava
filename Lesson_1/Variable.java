231028181e19fea07157d2e698133af58104c253
Zeoforex <dshoma853@gmail.com>
2021-07-04 16:17:35 +0300

    converter

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,19 +1,19 @@
 public class Variable{
-	public static void main(String[] args) {
-		char c = 'z';
-		boolean a = false;
-		int b = 14560;
-		double d = 2.44;
-		float f = 12.56f;
-		long l = 98560;
-		short s = 120;
-		byte by = 3;
-		System.out.println(c);
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(d);
-		System.out.println(f);
-		System.out.println(l);
-		System.out.println(by);
-	}
+    public static void main(String[] args) {
+        char c = 'z';
+        boolean a = false;
+        int b = 14560;
+        double d = 2.44;
+        float f = 12.56f;
+        long l = 9856035;
+        short s = 120;
+        byte by = 3;
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(by);
+    }
 }
