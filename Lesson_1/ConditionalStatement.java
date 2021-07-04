62678f24b5bfa4a7c11fa90e6d5f5aace2eb5f85
Zeoforex <dshoma853@gmail.com>
2021-07-04 16:15:59 +0300

    new file dz

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,29 +1,29 @@
 public class ConditionalStatement {
-	public static void main(String[] args) {
-		double height = 1.85;
-		char firstLetterName = 'K';
-		int age = 25;
-		boolean maleGender = true;
-		if (age>20) {
-			System.out.println("Молодой");
-		}
-		if (maleGender) {
-			System.out.println("Мужчина");
-		}
-		if (!maleGender) {
-			System.out.println("Женщина");
-		}
-		if (height < 1.80) {
-			System.out.println("Низкий");
-		} else {
-			System.out.println("Высокий");
-		}
-		if (firstLetterName == 'M') {
-			System.out.println("Первая буква это M");
-		} else if (firstLetterName == 'I') {
-			System.out.println("Первая буква это I");
-		} else {
-			System.out.println("Первая буква это K");
-		}
-	}
+    public static void main(String[] args) {
+        double height = 1.85;
+        char firstLetterName = 'K';
+        int age = 25;
+        boolean maleGender = true;
+        if (age>20) {
+            System.out.println("Молодой");
+        }
+        if (maleGender) {
+            System.out.println("Мужчина");
+        }
+        if (!maleGender) {
+            System.out.println("Женщина");
+        }
+        if (height < 1.80) {
+            System.out.println("Низкий");
+        } else {
+            System.out.println("Высокий");
+        }
+        if (firstLetterName == 'M') {
+            System.out.println("Первая буква это M");
+        } else if (firstLetterName == 'I') {
+            System.out.println("Первая буква это I");
+        } else {
+            System.out.println("Первая буква это K");
+        }
+    }
 }
