1492d1e8cc99430a4a87d2d904940c4a18953759
Zeoforex <dshoma853@gmail.com>
2021-07-06 12:21:46 +0300

    добавил задание про человека

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "Peter";
+    String gender = "мужской"
+    int age = 20; 
+    double height = 1.80;
+    int weight = 74;
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    void sit() {
+        System.out.println("sit down");
+    }
+
+    void run() {
+        System.out.println("run");
+    }
+
+    String speak() {
+        return "разговаривать с людьми"
+    }
+
+    void learnJava() {
+        System.out.println("learn Java");
+    }
+}
