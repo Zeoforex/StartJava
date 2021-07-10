5fe1809e4b4d121b3a1743d1f25dd0259eabd198
Zeoforex <dshoma853@gmail.com>
2021-07-10 14:39:15 +0300

    вроде все сделал по калькулятору

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -10,5 +10,28 @@ public class CalculatorTest {
 
         System.out.println(result);
 
+        String reply = "";
+        while (!reply.equals("yes") && !reply.equals("no")) {
+            System.out.println("Хотите продолжить вычисления? [yes/no]");
+            Scanner sc = new Scanner(System.in);
+            reply = sc.nextLine();
+        }
+        
+        while (reply.equals("yes")) {
+
+            int firstOne = calculator.inputFirstNumber();
+            char sign = calculator.signInput();
+            int secondOne = calculator.inputSecondNumber();
+            int resultOne = calculator.totalValue(firstOne,secondOne,sign);
+
+            System.out.println(resultOne);
+
+            reply = "";
+            while (!reply.equals("yes") && !reply.equals("no")) {
+                System.out.println("Хотите продолжить вычисления? [yes/no]");
+                Scanner sc = new Scanner(System.in);
+                reply = sc.nextLine();
+            }
+        }
     }
 }
