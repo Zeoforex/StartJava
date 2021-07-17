d6c2a959f670d0235929df9988d9a03bcaf54311
Zeoforex <dshoma853@gmail.com>
2021-07-17 16:55:24 +0300

    проблема с ^ знаком

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,21 +1,66 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
-    public int calculate(int firstNumber, int secondNumber, char sign) {
-        return switch (sign) {
-            case '+' -> firstNumber + secondNumber;
-            case '-' -> firstNumber - secondNumber;
-            case '*' -> firstNumber * secondNumber;
-            case '/' -> firstNumber / secondNumber;
-            case '^' -> {
-                int result = 1;
-                for (int i = 0; i < secondNumber; i++) {
-                    result *= firstNumber;
-                }
-                yield result;
+
+    public void start() {
+
+        String reply = "";
+        do {
+            System.out.println("Введите математическое выражение: Например(2+10) ");
+            Scanner sc = new Scanner(System.in);
+
+            String string = sc.nextLine();
+            String myText = string.replaceAll("\\s","");
+            String[] nums = myText.split("\\D");
+            String[] signs = myText.split("\\w");
+
+            int numFirst = Integer.parseInt(nums[0]);
+            char sign = signs[2].charAt(0);
+            int numSecond = Integer.parseInt(nums[1]);
+            calculate(numFirst, numSecond, sign);
+
+
+            while (!reply.equals("yes") && !reply.equals("no")) {
+                System.out.print("Хотите продолжить вычисления? [yes/no] ");
+                Scanner scanner = new Scanner(System.in);
+                reply = scanner.next();
             }
-            case '%' -> firstNumber % secondNumber;
-            default -> throw new IllegalStateException("Unexpected value: " + sign);
-        };
+        } while (reply.equals("yes"));
+    }
+
+        private void calculate(int num1, int num2, char sign) {
+            int result = 0;
+            switch (sign) {
+                case '+':
+                    result = num1 + num2;
+                    System.out.println(result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    System.out.println(result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    System.out.println(result);
+                    break;
+                case '/' :
+                    result = num1 / num2;
+                    System.out.println(result);
+                    break;
+                case '^' : {
+                    result = 1;
+                    for (int i = 0; i < num2; i++) {
+                        result *= num1;
+                    }
+                    System.out.println(result);
+                    break;
+                }
+                case '%':
+                    result = num1 % num2;
+                    System.out.println(result);
+                    break;
+            }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,27 +6,6 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        Scanner sc = new Scanner(System.in);
-
-        String reply = "";
-        do {
-            System.out.print("Введите первое число: ");
-            int firstNumber = sc.nextInt();
-
-            System.out.print("Введите знак математической операции: ");
-            char sign = sc.next().charAt(0);
-
-            System.out.print("Введите второе число: ");
-            int secondNumber = sc.nextInt();
-            int result = calculator.calculate(firstNumber, secondNumber, sign);
-
-            System.out.println(result);
-
-            while (!reply.equals("yes") && !reply.equals("no")) {
-                System.out.print("Хотите продолжить вычисления? [yes/no] ");
-                Scanner scanner = new Scanner(System.in);
-                reply = scanner.next();
-            }
-        } while (reply.equals("yes"));
+        calculator.start();
     }
 }
