31e2d984a76fc5010200eeb5ae9cd21c083b0707
Zeoforex <dshoma853@gmail.com>
2021-07-21 13:49:17 +0300

    поправил

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,52 +1,45 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import java.util.Scanner;
-
 public class Calculator {
 
-    public void start() {
+    private int firstNumber;
+    private int secondNumber;
+    private String sign;
 
-        String reply = "";
-        do {
-            System.out.println("Введите математическое выражение: Например(2 + 10) ");
-            Scanner sc = new Scanner(System.in);
-
-            String string = sc.nextLine();
-            String myText = string.replaceAll("\\s","");
-            String[] nums = myText.split("\\D+");
-            String[] signs = myText.split("\\d+");
-
-            int numFirst = Integer.parseInt(nums[0]);
-            char sign = signs[1].charAt(0);
-            int numSecond = Integer.parseInt(nums[1]);
-            calculate(numFirst, numSecond, sign);
-
-
-            while (!reply.equals("yes") && !reply.equals("no")) {
-                System.out.print("Хотите продолжить вычисления? [yes/no] ");
-                Scanner scanner = new Scanner(System.in);
-                reply = scanner.next();
+    public int calculate(String expression) {
+        rightInput(expression);
+        switch (sign) {
+            case "+" -> {
+                return firstNumber + secondNumber;
             }
-        } while (reply.equals("yes"));
-    }
-
-
-        private void calculate(int num1, int num2, char sign) {
-            int result;
-            switch (sign) {
-                case '+' -> result = num1 + num2;
-                case '-' -> result = num1 - num2;
-                case '*' -> result = num1 * num2;
-                case '/' -> result = num1 / num2;
-                case '^' -> {
-                    result = 1;
-                    for (int i = 0; i < num2; i++) {
-                        result *= num1;
+            case "-" -> {
+                return firstNumber - secondNumber;
+            }
+            case "*" -> {
+                return firstNumber * secondNumber;
+            }
+            case "/" -> {
+                return firstNumber / secondNumber;
+            }
+            case "^" -> {
+                int result = 1;
+                for (int i = 0; i < secondNumber; i++) {
+                        result *= firstNumber;
                     }
+                return result;
                 }
-                case '%' -> result = num1 % num2;
-                default -> throw new IllegalStateException("Unexpected value: " + sign);
+            case "%" -> {
+                return firstNumber % secondNumber;
             }
-            System.out.println("Твой ответ: " + result);
+            default -> throw new IllegalStateException("Unexpected value: " + sign);
+        }
     }
+
+    private void rightInput(String expression) {
+        String[] array = expression.split(" ");
+        firstNumber = Integer.parseInt(array[0]);
+        sign = array[1];
+        secondNumber = Integer.parseInt(array[2]);
+    }
+
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.start();
+        String reply="";
+        do {
+            System.out.println("Введите математическое выражение: Например(2 + 10) ");
+            Scanner sc = new Scanner(System.in);
+            Calculator calculator = new Calculator();
+            int result = calculator.calculate(sc.nextLine());
+            System.out.println("Ваш ответ равен " + result);
+
+
+            while (!reply.equals("yes") && !reply.equals("no")) {
+                System.out.print("Хотите продолжить вычисления? [yes/no] ");
+                Scanner scanner = new Scanner(System.in);
+                reply = scanner.next();
+            }
+        } while (reply.equals("yes"));
     }
 }
